581a3086cf9adf12db55f7fb2d74c38bba56eb88
Rasmus <rasmusloejee@gmail.com>
2020-07-28 20:24:51 +0200

    tests: service tests

--- Geometry.Logic.Tests/TriangleTypeServiceTests.cs
+++ Geometry.Logic.Tests/TriangleTypeServiceTests.cs
@@ -1,13 +1,35 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Geometry.Abstractions.Triangle;
+using Geometry.Logic.Resolvers;
+using Moq;
+using Xunit;
 
 namespace Geometry.Logic.Tests
 {
     public class TriangleTypeServiceTests
     {
         [Fact]
-        public void UnitTest()
+        public void Given_only_one_resolver_When_triangle_is_isosceles_type_Then_return_isosceles()
         {
+            // Arrange
+            var factoryMock = new Mock<ITriangleResolverFactory>();
+            factoryMock.Setup(x => x.CreateResolvers()).Returns(new List<ITriangleTypeResolver>
+            {
+                new IsoscelesResolver()
+            });
 
+            var triangleTypeService = new TriangleTypeService(factoryMock.Object);
+
+            var triangle = new Triangle(1, 1, 2);
+
+            var expectedTypes = new List<TriangleType> { TriangleType.Isosceles };
+
+            // Act
+            var types = triangleTypeService.GetTypes(triangle);
+
+            // Assert
+            types.Should().BeEquivalentTo(expectedTypes);
         }
     }
 }
